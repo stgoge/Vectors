4b89adee33d0480216553f7964e718af808f852a
Герман <stgoge@ya.ru>
2018-05-30 15:23:48 +0700

    инит и дименс и тест
    v 1.0

--- hw.js
+++ hw.js
@@ -1,16 +1,27 @@
 var vectorInit = function (dimension) {
-		if (typeof dimension === 'number') {
-			return new Array(Math.floor(dimension)).fill(0);
-		} else {
-			return dimension ? null : [0,0];
-		}
+	if (typeof dimension === 'number') {
+		return new Array(Math.floor(dimension)).fill(0);
+	} else {
+		return dimension ? null : [0,0];
 	}
+}
 
 var vectorDimension = function (vector) {
 	return vector ? vector.length : null;
 }
 
-var a = vectorInit(3);
-var d = vectorDimension(a);
+var doCompare = function (argA, argB, message) {
+	if (argA === argB) {
+		return true;
+	} 
+	console.log(message + '. expect: ' + argA + ', got: ' + argB);
+	return false;
+}
 
-console.log(d);
+doCompare(2, vectorDimension(vectorInit(2)),'2 dim creation test');
+doCompare(2, vectorDimension(vectorInit()),'undefined dim creation test');
+doCompare(2, vectorDimension(vectorInit(null)),'null dim creation test');
+doCompare(null, vectorDimension(vectorInit('23ebw')),'string dim creation test');
+
+
+
